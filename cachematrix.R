2b363522d55708fced7372fe9657fabdb053565b
AndyAdams216 <andrewadams216@gmail.com>
2014-04-26 17:26:52 +0100

    cachematrix implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Creates a matrix and its inverse
+makeCacheMatrix <- function(m_matrix=matrix()){
+    
+    m_inverse <- NULL
 
-## Write a short comment describing this function
+    # store the matrix and reset its inverse
+    set <- function(matrix) {
+        m_matrix <<- matrix
+        m_inverse <<- NULL
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
+    # access the matrix
+    get <- function() m_matrix
 
+    # sets the inverse of the matrix to 'inverse' if variable unbound 
+    # and available in environment
+    setInverse <- function(inverse) m_inverse <<- inverse
+
+    # access the inverse of the matrix
+    getInverse <- function() m_inverse
+
+    # return the cached matrix as a list
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Calcualtes the inverse of a given matrix and caches the result for future use
+cacheSolve <- function(cachedMatrix) {
+    
+    # attempt to get cached inverse
+    inverse <- cachedMatrix$getInverse()   
+    if(!is.null(inverse)) {
+        message("Found cached matrix inverse.")
+        return(inverse)
+    }
+    
+    # if no cached version calculate one
+    matrix <- cachedMatrix$get() 
+    # matrix must be square
+    if( ncol(matrix) != nrow(matrix) ){
+        message("Matrix is not square.")
+    }   
+    else if( det(matrix) == 0){
+        message("Cannot invert matrix with a determinant of zero.")
+    }
+    else{
+        # calculate inverse of matrix
+        inverse <- solve(matrix)
+        # cache matrix inversion
+        cachedMatrix$setInverse(inverse)
+        return(inverse)
+    }
+}   
